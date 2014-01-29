def1e7f4ee398b69036e8d05b4324213ed6b0e5f
Larry Weya <larryweya@gmail.com>
2014-01-29 15:56:24 +0300

    show user's clinics under /users route

--- whoahqa/__init__.py
+++ whoahqa/__init__.py
@@ -23,7 +23,7 @@ def main(global_config, **settings):
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('user_clinics', '/clinics/user/*traverse',
+    config.add_route('users', '/users/*traverse',
                      factory=UserFactory)
     config.add_route('clinics', '/clinics/*traverse',
                      factory=ClinicFactory)
