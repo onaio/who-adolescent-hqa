67d5857d31991c3725c4f7f84e086b09411b2367
Larry Weya <larryweya@gmail.com>
2014-02-11 17:54:54 +0300

    LW: added fabfile for deploys

---
+++ fabfile.py
@@ -0,0 +1,40 @@
+import os
+from fabric.api import local, cd, run, settings, env, prefix
+
+DEPLOYMENTS = {
+    'prod': {
+        'host_string': "ubuntu@whoahqa.ona.io",
+        'virtual_env': '/home/ubuntu/.virtualenvs/whoahqa_prod',
+        'project_dir': '/home/ubuntu/whoahqa',
+        'alembic_section': 'production'
+    },
+    'dev': {
+        'host_string': "vagrant@192.168.33.13",
+        'virtual_env': '/home/vagrant/.virtualenvs/whoahqa_dev',
+        'project_dir': '/home/vagrant/whoahqa'
+    }
+}
+
+
+def test(branch="master"):
+    local("git checkout %s" % branch)
+    local("python setup.py test -q")
+
+
+def deploy(deployment="prod", branch="master"):
+    env.update(DEPLOYMENTS[deployment])
+    virtual_env_command = 'source {}'.format(
+        os.path.join(env.virtual_env, 'bin', 'activate'))
+    test(branch)
+    with cd(env.project_dir):
+        run("git checkout {branch}".format(branch=branch))
+        run("git pull origin {branch}".format(branch=branch))
+
+        # run migrations
+        with prefix(virtual_env_command):
+            run("python setup.py install")
+            run("alembic -n {0} upgrade head".format(
+                env.get('alembic_section', 'alembic')))
+
+    # Reload uWSGI
+    run("/usr/local/bin/uwsgi --reload /var/run/whoahqa.pid")
