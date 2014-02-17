a73eab7f3c6fbd7b70dc01e8624e5199229f9748
Larry Weya <larryweya@gmail.com>
2014-02-17 23:10:26 +0300

    LW: user authorisation using db objects

--- whoahqa/security.py
+++ whoahqa/security.py
@@ -1,3 +1,8 @@
+from sqlalchemy.orm.exc import NoResultFound
+
+from whoahqa.models import User
+
+
 USERS = {
     1: 'admin',
     2: 'user1'
@@ -9,5 +14,11 @@ GROUPS = {
 
 
 def group_finder(userid, request):
-    if userid in USERS:
-        return GROUPS.get(userid, [])
+    try:
+        user = User.get(User.id == userid)
+    except NoResultFound:
+        return None
+    else:
+        groups = ["g:{}".format(g.name) for g in user.groups]
+        groups.append("u:{}".format(userid))
+        return groups
