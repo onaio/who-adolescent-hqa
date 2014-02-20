d8fe1d49f3c031dc071ef255ddcb50ad978ab891
Larry Weya <larryweya@gmail.com>
2014-02-20 14:48:21 +0300

    LW: generate code from hashid of clinic's id

--- whoahqa/utils.py
+++ whoahqa/utils.py
@@ -1,2 +1,7 @@
+from hashids import Hashids
+
+hashid = Hashids(min_length=4, alphabet='abcdefghijklmnpqrstuvwxyz123456789')
+
+
 def tuple_to_dict_list(key_tuple, value_tuples):
     return [dict(zip(key_tuple, c)) for c in value_tuples]
