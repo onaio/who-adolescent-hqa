e90c11e4d7644602d3477baa01c04273389c3981
royrutto <royrutto@gmail.com>
2014-03-03 13:44:05 +0300

    RR: Fix score table header on scroll and add table styles

--- whoahqa/static/scripts/custom.js
+++ whoahqa/static/scripts/custom.js
@@ -22,8 +22,68 @@ var Custom = function () {
         });
     };
 
+    var enablefloatThead = function() {
+
+       $.fn.fixheader = function (){
+          return this.each(function() {
+             var $this = $(this),
+                $t_fixed;
+             function init() {
+                $this.wrap('<div class="container" />');
+                $t_fixed = $this.clone();
+                $t_fixed.find("tbody").remove().end().addClass("fixed").insertBefore($this);
+                resizeFixed();
+             }
+             function resizeFixed() {
+                $t_fixed.find("th").each(function(index) {
+                   $(this).css("width",$this.find("th").eq(index).outerWidth()+"px");
+                });
+             }
+             function scrollFixed() {
+                var offset = $(this).scrollTop(),
+                tableOffsetTop = $this.offset().top,
+                tableOffsetBottom = tableOffsetTop + $this.height() - $this.find("thead").height();
+                if(offset < tableOffsetTop || offset > tableOffsetBottom)
+                   $t_fixed.hide();
+                else if(offset >= tableOffsetTop && offset <= tableOffsetBottom && $t_fixed.is(":hidden"))
+                   $t_fixed.show();
+             }
+             $(window).resize(resizeFixed);
+             $(window).scroll(scrollFixed);
+             init();
+          });
+       };
+       $(".characteristics").fixheader();
+       $(".score_indicators").fixheader();
+
+       var leftInit = $(".fixed").offset().left;
+       var initialLeftint = 926;
+
+       var top = $('.fixed').offset().top - parseFloat($('.fixed').css('margin-top').replace(/auto/, 0));
+
+       $('.score-summary-table2').scroll(function(event) {
+            var x = initialLeftint - $(this).scrollLeft();
+            var y = $(this).scrollTop();
+
+            // whether that's below the form
+            if (y >= top) {
+                // if so, ad the fixed class
+               $('.fixed').addClass('.fixed');
+            } else {
+                // otherwise remove it
+                $('.fixed').removeClass('.fixed');
+            }
+
+            $(".fixed").offset({
+                left: x + leftInit
+            });
+        });
+
+    };
+
     return {
-        addCheckAllListener: addCheckAllListener
+        addCheckAllListener: addCheckAllListener,
+        enablefloatThead: enablefloatThead
     }
 }();
 
