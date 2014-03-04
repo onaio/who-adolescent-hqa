83cc28495ef35a7efdc1907c90da67b6f24b8dda
Larry Weya <larryweya@gmail.com>
2014-03-04 12:30:32 +0300

    LW: added global enketo object for easy configuration

--- whoahqa/utils.py
+++ whoahqa/utils.py
@@ -1,6 +1,9 @@
 from hashids import Hashids
+from pyenketo import Enketo
+
 
 hashid = Hashids(min_length=4, alphabet='abcdefghijklmnpqrstuvwxyz123456789')
+enketo = Enketo()
 
 
 def tuple_to_dict_list(key_tuple, value_tuples):
