cd0367f8a1e050990f864014de4242dc61757f64
Larry Weya <larryweya@gmail.com>
2014-03-10 16:25:38 +0300

    LW: reporting period create

--- whoahqa/tests/views/test_reporting_periods.py
+++ whoahqa/tests/views/test_reporting_periods.py
@@ -1,4 +1,44 @@
-from whoahqa.tests.test_base import FunctionalTestBase
+import deform
+
+from pyramid import testing
+from webob.multidict import MultiDict
+
+from whoahqa.views.reporting_periods import ReportingPeriodViews
+from whoahqa.tests.test_base import IntegrationTestBase, FunctionalTestBase
+
+
+class TestReportingPeriods(IntegrationTestBase):
+    def setUp(self):
+        super(TestReportingPeriods, self).setUp()
+        self.request = testing.DummyRequest()
+        self.view = ReportingPeriodViews(self.request)
+
+    def test_create_POST_creates_period(self):
+        self.request.method = "POST"
+        self.request.POST = MultiDict([
+            ('title', "2013/2014"),
+            ('start_date', '2014-02-01'),
+            ('end_date', '2015-02-01')
+        ])
+        response = self.view.create()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.location,
+            self.request.route_url('periods', traverse=()))
+
+    def test_create_invalid_POST_response(self):
+        self.request.method = "POST"
+        self.request.POST = MultiDict([
+            ('title', "2013/2014"),
+            ('start_date', '2014-02-01'),
+        ])
+        response = self.view.create()
+        self.assertIn('form', response)
+
+    def test_create_GET_response(self):
+        response = self.view.create()
+        self.assertIn('form', response)
+        self.assertIsInstance(response['form'], deform.Form)
 
 
 class TestReportingPeriodsFunctional(FunctionalTestBase):
