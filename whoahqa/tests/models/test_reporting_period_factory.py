39248edea1b453ecdac056193e5457b1aad12ac2
Larry Weya <larryweya@gmail.com>
2014-03-10 14:22:39 +0300

    LW: use period's id to retrieve it from the factory

--- whoahqa/tests/models/test_reporting_period_factory.py
+++ whoahqa/tests/models/test_reporting_period_factory.py
@@ -11,23 +11,24 @@ from whoahqa.tests.test_base import TestBase
 
 
 class TestReportingPeriodFactory(TestBase):
-    def test_get_item_retrieves_by_slug(self):
-        slug = '2014-2015'
+    def test_get_item_retrieves_by_id(self):
         period = ReportingPeriod(
-            title="2014/2015", slug=slug,
+            title="2014/2015",
             start_date=datetime.date(2014, 2, 1),
             end_date=datetime.date(2015, 2, 1))
         DBSession.add(period)
+        DBSession.flush()
+        id = period.id
 
         factory = ReportingPeriodFactory(
             testing.DummyRequest())
-        period = factory.__getitem__(slug)
-        self.assertEqual(period.slug, slug)
+        period = factory.__getitem__(id)
+        self.assertEqual(period.id, id)
         self.assertEqual(period.__parent__, factory)
-        self.assertEqual(period.__name__, slug)
+        self.assertEqual(period.__name__, id)
 
-    def test_get_item_throws_key_error_if_non_existent_slug(self):
+    def test_get_item_throws_key_error_if_non_existent_id(self):
         factory = ReportingPeriodFactory(
             testing.DummyRequest())
-        self.assertRaises(KeyError, factory.__getitem__, 'non-existent')
+        self.assertRaises(KeyError, factory.__getitem__, '0')
 
