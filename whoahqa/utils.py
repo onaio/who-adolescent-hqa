1a9505ca2abd9397bf27a69f5559875ea24a3dff
Larry Weya <larryweya@gmail.com>
2014-03-11 13:23:26 +0300

    LW: custom filter to format dates based on locale

--- whoahqa/utils.py
+++ whoahqa/utils.py
@@ -1,7 +1,8 @@
 from hashids import Hashids
 from pyenketo import Enketo
+from babel.dates import format_date
 
-from pyramid.i18n import TranslationStringFactory
+from pyramid.i18n import TranslationStringFactory, get_localizer
 
 
 translation_string_factory = TranslationStringFactory('who-ahqa')
@@ -11,3 +12,8 @@ enketo = Enketo()
 
 def tuple_to_dict_list(key_tuple, value_tuples):
     return [dict(zip(key_tuple, c)) for c in value_tuples]
+
+
+def format_locale_date(value, format_def, request):
+    localizer = get_localizer(request)
+    return format_date(value, format_def, locale=localizer.locale_name)
