8b070a59d9a2a71f12b7f625db6280b79fa4adc6
Larry Weya <larryweya@gmail.com>
2014-03-20 13:55:35 +0300

    LW: fix home page redirect to include reporting periods

--- whoahqa/tests/views/test_default_views.py
+++ whoahqa/tests/views/test_default_views.py
@@ -1,3 +1,4 @@
+from whoahqa.models import OnaUser
 from whoahqa.tests import FunctionalTestBase
 
 
@@ -5,9 +6,18 @@ class TestDefaultViewsFunctional(FunctionalTestBase):
     def test_redirects_to_clinics_when_authenticated(self):
         self.setup_test_data()
         url = self.request.route_url('default')
+        ona_user = OnaUser.get(OnaUser.username == 'manager_a')
         headers = self._login_user("manager_a")
         response = self.testapp.get(url, headers=headers)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
-            response.headers['Location'],
-            self.request.route_url('clinics', traverse=()))
+            response.location,
+            self.request.route_url(
+                'users',
+                traverse=(ona_user.user.id, 'select-period'),
+                _query={
+                    'came_from': self.request.route_path(
+                        'users', traverse=(
+                            ona_user.user.id, '{period_id}', 'clinics'))}))
+        response = response.follow(headers=headers)
+        self.assertEqual(response.status_code, 200)
