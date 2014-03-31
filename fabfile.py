7889e114a379e84d107e1c1a78f4e059e5b04d11
Larry Weya <larryweya@gmail.com>
2014-03-31 13:27:26 +0300

    LW: moved testing to server side

--- fabfile.py
+++ fabfile.py
@@ -16,23 +16,18 @@ DEPLOYMENTS = {
 }
 
 
-def test(branch="master"):
-    local("git checkout %s" % branch)
-    local("python setup.py test -q")
-
-
 def deploy(deployment="prod", branch="master"):
     env.update(DEPLOYMENTS[deployment])
     virtual_env_command = 'source {}'.format(
         os.path.join(env.virtual_env, 'bin', 'activate'))
-    test(branch)
     with cd(env.project_dir):
         run("git checkout {branch}".format(branch=branch))
         run("git pull origin {branch}".format(branch=branch))
 
-        # run migrations
         with prefix(virtual_env_command):
+            run("python setup.py test -q")
             run("python setup.py install")
+            # run migrations
             run("alembic -n {0} upgrade head".format(
                 env.get('alembic_section', 'alembic')))
             run("python setup.py compile_catalog")
