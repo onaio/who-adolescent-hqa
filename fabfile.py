d10dcaa77f0b0e74b24a949443bb1b5f55f8eaa9
Larry Weya <larryweya@gmail.com>
2014-04-04 10:46:01 +0300

    LW: separate venc fot tests

--- fabfile.py
+++ fabfile.py
@@ -5,27 +5,36 @@ DEPLOYMENTS = {
     'prod': {
         'host_string': "ubuntu@whoahqa.ona.io",
         'virtual_env': '/home/ubuntu/.virtualenvs/whoahqa_prod',
+        'test_virtual_env': '/home/ubuntu/.virtualenvs/whoahqa_test',
         'project_dir': '/home/ubuntu/whoahqa',
         'alembic_section': 'production'
     },
     'dev': {
         'host_string': "vagrant@192.168.33.13",
         'virtual_env': '/home/vagrant/.virtualenvs/whoahqa_dev',
+        'test_virtual_env': '/home/vagrant/.virtualenvs/whoahqa_test',
         'project_dir': '/home/vagrant/whoahqa'
     }
 }
 
 
+def get_virtual_env_command(virtual_env_path):
+    return 'source {}'.format(
+        os.path.join(virtual_env_path, 'bin', 'activate'))
+
+
 def deploy(deployment="prod", branch="master"):
     env.update(DEPLOYMENTS[deployment])
-    virtual_env_command = 'source {}'.format(
-        os.path.join(env.virtual_env, 'bin', 'activate'))
+    virtual_env_command = get_virtual_env_command(env.virtual_env)
+    test_virtual_env_command = get_virtual_env_command(env.test_virtual_env)
     with cd(env.project_dir):
         run("git checkout {branch}".format(branch=branch))
         run("git pull origin {branch}".format(branch=branch))
 
-        with prefix(virtual_env_command):
+        with prefix(test_virtual_env_command):
             run("python setup.py test -q")
+
+        with prefix(virtual_env_command):
             run("python setup.py install")
             # run migrations
             run("alembic -n {0} upgrade head".format(
