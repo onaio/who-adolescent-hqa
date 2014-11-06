e899df79a1205c7a2420ef920b78d1749f807f55
geoffreymuchai <geoffreymuchai@gmail.com>
2014-11-06 15:50:30 +0300

    GM: Added view for changing locales and tests

--- whoahqa/views/__init__.py
+++ whoahqa/views/__init__.py
@@ -1,25 +1,7 @@
-from pyramid.httpexceptions import (
-    HTTPFound,
-)
-
-from pyramid.view import view_config  # noqa
-
-from request_methods import get_request_user, can_list_clinics  # noqa
-from auth import oauth_authorize, oauth_callback  # noqa
-from clinics import ClinicViews  # noqa
-from users import UserViews  # noqa
-from submissions import SubmissionViews  # noqa
-
-
-@view_config(route_name='default')
-def default(request):
-    ona_user = request.ona_user
-
-    return HTTPFound(
-        request.route_url(
-            'users',
-            traverse=(ona_user.user.id, 'select-period'),
-            _query={
-                'came_from': request.route_path(
-                    'users',
-                    traverse=(ona_user.user.id, '{period_id}', 'clinics'))}))
+from whoahqa.views.auth import oauth_authorize, oauth_callback  # noqa
+from whoahqa.views.clinics import ClinicViews  # noqa
+from whoahqa.views.default_views import default  # noqa
+from whoahqa.views.default_views import set_locale  # noqa
+from whoahqa.views.request_methods import get_request_user, can_list_clinics  # noqa
+from whoahqa.views.submissions import SubmissionViews  # noqa
+from whoahqa.views.users import UserViews  # noqa
---
+++ whoahqa/views/default_views.py
@@ -0,0 +1,47 @@
+from pyramid.httpexceptions import (
+    HTTPFound,
+)
+
+from whoahqa.constants import AVAILABLE_LANGUAGES
+from whoahqa.constants import permissions as perms
+from pyramid.view import view_config
+
+
+@view_config(route_name='default')
+def default(request):
+    ona_user = request.ona_user
+
+    user_setting = ona_user.user.settings
+    if user_setting:
+        request.response.set_cookie(
+            '_LOCALE_', user_setting.language)
+
+    return HTTPFound(
+        request.route_url(
+            'users',
+            traverse=(ona_user.user.id, 'select-period'),
+            _query={
+                'came_from': request.route_path(
+                    'users',
+                    traverse=(ona_user.user.id, '{period_id}', 'clinics'))}))
+
+
+@view_config(route_name='locale',
+             permission=perms.SHOW,
+             renderer='locale.jinja2')
+def set_locale(request):
+    ona_user = request.ona_user
+    user_settings = ona_user.user.settings
+
+    available_languages = AVAILABLE_LANGUAGES
+
+    if request.method == "POST":
+        locale = request.POST.get("locale", "")
+        if locale and locale in available_languages:
+            user_settings.language = locale
+            user_settings.save()
+            request.response.set_cookie('_LOCALE_', locale)
+
+    return {
+        "available_languages": available_languages,
+        "user_settings": user_settings}
