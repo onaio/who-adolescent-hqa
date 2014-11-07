e93e85c4adcf67890d9c2b1b1710d3747c1776c8
geoffreymuchai <geoffreymuchai@gmail.com>
2014-11-07 17:10:48 +0300

    GM: Refactored characteristic constants

---
+++ whoahqa/constants/brazil_characteristics.py
@@ -0,0 +1,12 @@
+from whoahqa.constants.base_characteristics import *  # noqa
+
+# filter CHARACTERISTICS to remove CHARACTERISTIC 4
+
+KEY_INDICATORS = [
+    (EQUITABLE, (ONE, TWO, THREE)),
+    (ACCESSIBLE, (FIVE, SIX, SEVEN, EIGHT)),
+    (ACCEPTABLE, (NINE, TEN, ELEVEN, TWELVE, THIRTEEN, FOURTEEN,
+                  FIFTEEN)),
+    (APPROPRIATE, (SIXTEEN, )),
+    (EFFECTIVE, (SEVENTEEN, EIGHTEEN, NINETEEN, TWENTY))
+]
