230240b2bb4c15ce7bc461f9284c198759005933
geoffreymuchai <geoffreymuchai@gmail.com>
2014-11-10 11:39:02 +0300

    GM: Fix Ch. 9 update and add health care provider tool changes

--- whoahqa/constants/brazil_characteristics.py
+++ whoahqa/constants/brazil_characteristics.py
@@ -14,7 +14,7 @@ KEY_INDICATORS = [
 ]
 
 CHARACTERISTIC_MAPPING[NINE][ADOLESCENT_CLIENT].update(
-    'characteristic_nine/ch9_q5')
+    {'characteristic_nine/ch9_q5'})
 
 CHARACTERISTIC_MAPPING[TWELVE][ADOLESCENT_CLIENT] = {
     'characteristic_twelve/ch12_q1',
@@ -29,4 +29,49 @@ CHARACTERISTIC_MAPPING[FOURTEEN][ADOLESCENT_CLIENT] = {
     'characteristic_twelve/ch14_q1_yes2',
     'characteristic_twelve/ch14_q1_yes3',
     'characteristic_twelve/ch14_q1_yes5_Ptg'
+}
+
+CHARACTERISTIC_MAPPING[SIXTEEN][HEALTH_CARE_PROVIDER] = {
+    'characteristic_sixteen/ch16_q1',
+    'characteristic_sixteen/ch16_q2',
+    'characteristic_sixteen/ch16_q3',
+    'characteristic_sixteen/ch16_q4',
+    'characteristic_sixteen/ch16_q5',
+    'characteristic_sixteen/ch16_q6',
+    'characteristic_sixteen/ch16_q9',
+    'characteristic_sixteen/ch16_q10',
+    'characteristic_sixteen/ch16_q11',
+    'characteristic_sixteen/ch16_q12',
+    'characteristic_sixteen/ch16_q13',
+    'characteristic_sixteen/ch16_q14'
+}
+
+CHARACTERISTIC_MAPPING[SEVENTEEN][HEALTH_CARE_PROVIDER] = {
+    'characteristic_seventeen/ch17_q1',
+    'characteristic_seventeen/ch17_q2',
+    'characteristic_seventeen/ch17_q3',
+    'characteristic_seventeen/ch17_q4',
+    'characteristic_seventeen/ch17_q5',
+    'characteristic_seventeen/ch17_q6',
+    'characteristic_seventeen/ch17_q9',
+    'characteristic_seventeen/ch17_q10',
+    'characteristic_seventeen/ch17_q11',
+    'characteristic_seventeen/ch17_q12',
+    'characteristic_seventeen/ch17_q13',
+    'characteristic_seventeen/ch17_q14',
+    'characteristic_seventeen/ch17_q15'
+}
+
+CHARACTERISTIC_MAPPING[EIGHTEEN][HEALTH_CARE_PROVIDER] = {
+    'characteristic_eighteen/ch18_q1',
+    'characteristic_eighteen/ch18_q2',
+    'characteristic_eighteen/ch18_q3',
+    'characteristic_eighteen/ch18_q4',
+    'characteristic_eighteen/ch18_q5',
+    'characteristic_eighteen/ch18_q6',
+    'characteristic_eighteen/ch18_q9',
+    'characteristic_eighteen/ch18_q10',
+    'characteristic_eighteen/ch18_q11',
+    'characteristic_eighteen/ch18_q12',
+    'characteristic_eighteen/ch18_q13'
 }
