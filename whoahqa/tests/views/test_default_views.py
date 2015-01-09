b9407859a3135f00b9565ca6899f1a8ec8873dd8
geoffreymuchai <geoffreymuchai@gmail.com>
2015-01-09 15:28:34 +0300

    GM: Fixed failing tests

--- whoahqa/tests/views/test_default_views.py
+++ whoahqa/tests/views/test_default_views.py
@@ -1,4 +1,3 @@
-from whoahqa.models import OnaUser
 from whoahqa.tests import FunctionalTestBase
 
 
@@ -6,18 +5,9 @@ class TestDefaultViewsFunctional(FunctionalTestBase):
     def test_redirects_to_clinics_when_authenticated(self):
         self.setup_test_data()
         url = self.request.route_url('default')
-        ona_user = OnaUser.get(OnaUser.username == 'manager_a')
         headers = self._login_user("manager_a")
         response = self.testapp.get(url, headers=headers)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
             response.location,
-            self.request.route_url(
-                'users',
-                traverse=(ona_user.user.id, 'select-period'),
-                _query={
-                    'came_from': self.request.route_path(
-                        'users', traverse=(
-                            ona_user.user.id, '{period_id}', 'clinics'))}))
-        response = response.follow(headers=headers)
-        self.assertEqual(response.status_code, 200)
+            self.request.route_url('clinics', traverse=()))
