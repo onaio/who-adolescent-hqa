d7f3acf7426f5660481e104f76ffbd9cd1ca0e51
geoffreymuchai <geoffreymuchai@gmail.com>
2015-01-09 15:29:01 +0300

    GM: Added Group list

--- whoahqa/constants/groups.py
+++ whoahqa/constants/groups.py
@@ -2,3 +2,6 @@ USER = 'user'
 SUPER_USER = 'su'
 CLINIC_MANAGER = 'clinic_manager'
 MUNICIPALITY_MANAGER = 'municipality_manager'
+STATE_OFFICIAL = 'state_official'
+
+GROUPS = [CLINIC_MANAGER, MUNICIPALITY_MANAGER, STATE_OFFICIAL]
