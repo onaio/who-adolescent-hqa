58d2f1e61451e842b78a4e4952aa2be9bf823d56
geoffreymuchai <geoffreymuchai@gmail.com>
2015-01-13 16:47:28 +0300

    GM: Fix permission issue when listing clinic scores

--- whoahqa/constants/permissions.py
+++ whoahqa/constants/permissions.py
@@ -1,10 +1,14 @@
 AUTHENTICATED = 'authenticated'
 SUPER_USER = 'su'
 
+# Clinic permissions
 CAN_LIST_CLINICS = 'list-clinics'
 CAN_VIEW_CLINICS = 'view-clinics'
 CAN_EDIT_CLINICS = 'edit-clinics'
 
+# Period permissions
+
+CAN_CREATE_PERIOD = 'can-create-period'
 
 # Municipality Manager Permissions
 CAN_VIEW_MUNICIPALITY = 'view-municipality'
