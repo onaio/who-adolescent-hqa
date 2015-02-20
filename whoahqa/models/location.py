fa82c5b20a1723952a2a4bce35a18f3ce931e592
geoffreymuchai <geoffreymuchai@gmail.com>
2015-02-20 10:37:52 +0300

    GM: Add LocationFactory

--- whoahqa/models/location.py
+++ whoahqa/models/location.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pyramid.security import (
     Allow,
     Authenticated,
@@ -12,7 +13,10 @@ from sqlalchemy import (
     ForeignKey
 )
 from sqlalchemy.orm import relationship
-from whoahqa.models import Base
+from sqlalchemy.orm.exc import NoResultFound
+from whoahqa.models import (
+    Base,
+    BaseModelFactory)
 from whoahqa.constants import permissions as perms, groups
 
 
@@ -36,6 +40,9 @@ class Location(Base):
         'polymorphic_identity': 'location'
     }
 
+    def key_indicators(self, period):
+        return defaultdict(int)
+
 
 class Municipality(Location):
     __mapper_args__ = {
@@ -47,6 +54,11 @@ class Municipality(Location):
         (Allow, Authenticated, perms.CAN_VIEW_MUNICIPALITY)
     ]
 
+    def get_url(self, request, period):
+        return request.route_url('municipalities',
+                                 traverse=(self.id),
+                                 _query={'period': period.id})
+
 
 class State(Location):
     __mapper_args__ = {
@@ -57,3 +69,16 @@ class State(Location):
         (Allow, groups.SUPER_USER, ALL_PERMISSIONS),
         (Allow, Authenticated, perms.CAN_VIEW_STATE)
     ]
+
+
+class LocationFactory(BaseModelFactory):
+    def __getitem__(self, item):
+        try:
+            location_id = int(item)
+            location = Location.get(Location.id == location_id)
+        except(ValueError, NoResultFound):
+            raise KeyError
+        else:
+            location.__parent__ = self
+            location.__name__ = item
+            return location
