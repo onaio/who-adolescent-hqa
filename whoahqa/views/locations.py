175c1218276391c2504b922dbe7afa2ae898d8d4
geoffreymuchai <geoffreymuchai@gmail.com>
2015-02-20 10:39:39 +0300

    GM: Add municipality views

---
+++ whoahqa/views/locations.py
@@ -0,0 +1,45 @@
+from pyramid.view import (
+    view_config,
+    view_defaults,
+)
+
+from whoahqa.constants import characteristics as constants
+from whoahqa.constants import permissions as perms
+from whoahqa.models import (
+    LocationFactory,
+    Municipality,
+    ReportingPeriod)
+from whoahqa.views.base import BaseClassViews
+
+
+@view_defaults(route_name='municipalities',
+               permission=perms.CAN_LIST_MUNICIPALITY)
+class MunicipalityViews(BaseClassViews):
+    @view_config(name='',
+                 context=LocationFactory,
+                 renderer='location_list.jinja2',
+                 request_method='GET')
+    def index(self):
+        period = ReportingPeriod.newest()
+        return {
+            'locations': Municipality.all(),
+            'period': period,
+            'key_indicators_key_labels': constants.INDICATOR_LABELS,
+        }
+
+    @view_config(name='',
+                 context=Municipality,
+                 renderer='clinics_summary.jinja2',
+                 request_method='GET')
+    def show(self):
+        municipality = self.request.context
+        clinics = municipality.clinics
+        # TODO determine period based on what user selected.
+        period = ReportingPeriod.newest()
+
+        return {
+            'locations': clinics,
+            'parent': municipality,
+            'period': period,
+            'key_indicators_key_labels': constants.INDICATOR_LABELS,
+        }
