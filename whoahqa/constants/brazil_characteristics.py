d529d338ea5b7dfa19456698120c2c4c7ef0e4b2
geoffreymuchai <geoffreymuchai@gmail.com>
2015-03-12 15:56:50 +0300

    GM: Updated brazil characteristics scores mapping

--- whoahqa/constants/brazil_characteristics.py
+++ whoahqa/constants/brazil_characteristics.py
@@ -14,149 +14,84 @@ KEY_INDICATORS = [
     (EFFECTIVE, (EIGHTEEN, NINETEEN, TWENTY))
 ]
 
-CHARACTERISTIC_MAPPING[NINE][ADOLESCENT_CLIENT].update(
-    {'characteristic_nine/ch9_q5'})
-
-CHARACTERISTIC_MAPPING[TWELVE][ADOLESCENT_CLIENT] = {
-    'characteristic_twelve/ch12_q1',
-    'characteristic_twelve/ch12_q2_yes',
-    'characteristic_twelve/ch12_q2_yes2'
+# Characteristic 4 has been removed since health care in brazil is free
+# Characteristic 17 has also been removed.
+CHARACTERISTIC_MAPPING = {
+    ONE: {
+        'characteristic_one/ch1_score': [ADOLESCENT_CLIENT,
+                                         HEALTH_CARE_PROVIDER,
+                                         HEALTH_FACILITY_MANAGER,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    TWO: {
+        'characteristic_two/ch2_score': [ADOLESCENT_CLIENT,
+                                         HEALTH_CARE_PROVIDER,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    THREE: {
+        'characteristic_two/ch3_score': [ADOLESCENT_CLIENT,
+                                         SUPPORT_STAFF,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    FIVE: {
+        'characteristic_two/ch5_score': [ADOLESCENT_CLIENT,
+                                         HEALTH_FACILITY_MANAGER,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    SIX: {
+        'characteristic_two/ch6_score': [ADOLESCENT_CLIENT,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    SEVEN: {
+        'characteristic_two/ch7_score': [ADOLESCENT_CLIENT,
+                                         HEALTH_CARE_PROVIDER,
+                                         COMMUNITY_MEMBER,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    EIGHT: {
+        'characteristic_two/ch8_score': [ADOLESCENT_CLIENT,
+                                         HEALTH_FACILITY_MANAGER,
+                                         OUTREACH_WORKER,
+                                         ADOLESCENT_IN_COMMUNITY]},
+    NINE: {
+        'characteristic_two/ch9_score': [ADOLESCENT_CLIENT,
+                                         HEALTH_CARE_PROVIDER,
+                                         HEALTH_FACILITY_MANAGER,
+                                         OBSERVATION_GUIDE]},
+    TEN: {
+        'characteristic_two/ch10_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_CARE_PROVIDER,
+                                          HEALTH_FACILITY_MANAGER,
+                                          OBSERVATION_GUIDE]},
+    ELEVEN: {
+        'characteristic_two/ch11_score': [ADOLESCENT_CLIENT,
+                                          OBSERVATION_GUIDE]},
+    TWELVE: {
+        'characteristic_two/ch12_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_CARE_PROVIDER,
+                                          SUPPORT_STAFF,
+                                          OBSERVATION_GUIDE]},
+    THIRTEEN: {
+        'characteristic_two/ch13_score': [ADOLESCENT_CLIENT,
+                                          OBSERVATION_GUIDE]},
+    FOURTEEN: {
+        'characteristic_two/ch14_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_FACILITY_MANAGER,
+                                          OBSERVATION_GUIDE]},
+    FIFTEEN: {
+        'characteristic_two/ch15_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_FACILITY_MANAGER]},
+    SIXTEEN: {
+        'characteristic_two/ch16_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_CARE_PROVIDER,
+                                          HEALTH_FACILITY_MANAGER]},
+    EIGHTEEN: {
+        'characteristic_two/ch18_score': [HEALTH_CARE_PROVIDER,
+                                          HEALTH_FACILITY_MANAGER,
+                                          OBSERVATION_GUIDE]},
+    NINETEEN: {
+        'characteristic_two/ch19_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_CARE_PROVIDER]},
+    TWENTY: {
+        'characteristic_two/ch20_score': [ADOLESCENT_CLIENT,
+                                          HEALTH_CARE_PROVIDER,
+                                          HEALTH_FACILITY_MANAGER,
+                                          OBSERVATION_GUIDE]}
 }
-
-CHARACTERISTIC_MAPPING[FOURTEEN][ADOLESCENT_CLIENT] = {
-    'characteristic_fourteen/ch14_q1',
-    'characteristic_fourteen/ch14_q1_Ptg',
-    'characteristic_fourteen/ch14_q1_yes',
-    'characteristic_fourteen/ch14_q1_yes2',
-    'characteristic_fourteen/ch14_q1_yes3',
-    'characteristic_fourteen/ch14_q1_yes5_Ptg'
-}
-
-CHARACTERISTIC_MAPPING[SIXTEEN][HEALTH_CARE_PROVIDER] = {
-    'characteristic_sixteen/ch16_q1',
-    'characteristic_sixteen/ch16_q2',
-    'characteristic_sixteen/ch16_q3',
-    'characteristic_sixteen/ch16_q4',
-    'characteristic_sixteen/ch16_q5',
-    'characteristic_sixteen/ch16_q6',
-    'characteristic_sixteen/ch16_q9',
-    'characteristic_sixteen/ch16_q10',
-    'characteristic_sixteen/ch16_q11',
-    'characteristic_sixteen/ch16_q12',
-    'characteristic_sixteen/ch16_q13',
-    'characteristic_sixteen/ch16_q14'
-}
-
-CHARACTERISTIC_MAPPING[SEVENTEEN][HEALTH_CARE_PROVIDER] = {
-    'characteristic_seventeen/ch17_q1',
-    'characteristic_seventeen/ch17_q2',
-    'characteristic_seventeen/ch17_q3',
-    'characteristic_seventeen/ch17_q4',
-    'characteristic_seventeen/ch17_q5',
-    'characteristic_seventeen/ch17_q6',
-    'characteristic_seventeen/ch17_q9',
-    'characteristic_seventeen/ch17_q10',
-    'characteristic_seventeen/ch17_q11',
-    'characteristic_seventeen/ch17_q12',
-    'characteristic_seventeen/ch17_q13',
-    'characteristic_seventeen/ch17_q14',
-    'characteristic_seventeen/ch17_q15'
-}
-
-CHARACTERISTIC_MAPPING[EIGHTEEN][HEALTH_CARE_PROVIDER] = {
-    'characteristic_eighteen/ch18_q1',
-    'characteristic_eighteen/ch18_q2',
-    'characteristic_eighteen/ch18_q3',
-    'characteristic_eighteen/ch18_q4',
-    'characteristic_eighteen/ch18_q5',
-    'characteristic_eighteen/ch18_q6',
-    'characteristic_eighteen/ch18_q9',
-    'characteristic_eighteen/ch18_q10',
-    'characteristic_eighteen/ch18_q11',
-    'characteristic_eighteen/ch18_q12',
-    'characteristic_eighteen/ch18_q13'
-}
-
-CHARACTERISTIC_MAPPING[FOURTEEN][HEALTH_FACILITY_MANAGER].update(
-    {'characteristic_fourteen/ch14_q3Ptg'})
-
-CHARACTERISTIC_MAPPING[SIXTEEN][HEALTH_FACILITY_MANAGER] = {
-    'characteristic_sixteen/ch16_q1',
-    'characteristic_sixteen/ch16_q2',
-    'characteristic_sixteen/ch16_q3',
-    'characteristic_sixteen/ch16_q4',
-    'characteristic_sixteen/ch16_q5',
-    'characteristic_sixteen/ch16_q6',
-    'characteristic_sixteen/ch16_q9Eng',
-    'characteristic_sixteen/ch16_q10',
-    'characteristic_sixteen/ch16_q11',
-    'characteristic_sixteen/ch16_q12',
-    'characteristic_sixteen/ch16_q13',
-    'characteristic_sixteen/ch16_q14'
-}
-
-CHARACTERISTIC_MAPPING[SEVENTEEN][HEALTH_FACILITY_MANAGER] = {
-    'characteristic_seventeen/ch17_q1',
-    'characteristic_seventeen/ch17_q2',
-    'characteristic_seventeen/ch17_q3',
-    'characteristic_seventeen/ch17_q4',
-    'characteristic_seventeen/ch17_q5',
-    'characteristic_seventeen/ch17_q6',
-    'characteristic_seventeen/ch17_q9Eng',
-    'characteristic_seventeen/ch17_q10',
-    'characteristic_seventeen/ch17_q11',
-    'characteristic_seventeen/ch17_q12',
-    'characteristic_seventeen/ch17_q13'
-}
-
-CHARACTERISTIC_MAPPING[EIGHTEEN][HEALTH_FACILITY_MANAGER] = {
-    'characteristic_eighteen/ch18_q1',
-    'characteristic_eighteen/ch18_q2',
-    'characteristic_eighteen/ch18_q3',
-    'characteristic_eighteen/ch18_q4',
-    'characteristic_eighteen/ch18_q5',
-    'characteristic_eighteen/ch18_q6',
-    'characteristic_eighteen/ch18_q9Eng',
-    'characteristic_eighteen/ch18_q10',
-    'characteristic_eighteen/ch18_q11',
-    'characteristic_eighteen/ch18_q12',
-    'characteristic_eighteen/ch18_q13'
-}
-
-CHARACTERISTIC_MAPPING[SEVENTEEN][OBSERVATION_GUIDE] = {
-    'characteristic_seventeen/ch17_q1',
-    'characteristic_seventeen/ch17_q2',
-    'characteristic_seventeen/ch17_q3',
-    'characteristic_seventeen/ch17_q4',
-    'characteristic_seventeen/ch17_q5',
-    'characteristic_seventeen/ch17_q6',
-    'characteristic_seventeen/ch17_q9Eng',
-    'characteristic_seventeen/ch17_q10',
-    'characteristic_seventeen/ch17_q11',
-    'characteristic_seventeen/ch17_q12',
-    'characteristic_seventeen/ch17_q13',
-    'characteristic_seventeen/ch17_q14',
-    'characteristic_seventeen/ch17_q15',
-    'characteristic_seventeen/ch17_q16',
-    'characteristic_seventeen/ch17_q17'
-}
-
-CHARACTERISTIC_MAPPING[EIGHTEEN][OBSERVATION_GUIDE] = {
-    'characteristic_eighteen/ch18_q1',
-    'characteristic_eighteen/ch18_q2',
-    'characteristic_eighteen/ch18_q3',
-    'characteristic_eighteen/ch18_q4',
-    'characteristic_eighteen/ch18_q5',
-    'characteristic_eighteen/ch18_q6',
-    'characteristic_eighteen/ch18_q9Eng',
-    'characteristic_eighteen/ch18_q10',
-    'characteristic_eighteen/ch18_q11',
-    'characteristic_eighteen/ch18_q12',
-    'characteristic_eighteen/ch18_q13'
-}
-
-CHARACTERISTIC_MAPPING[TWENTY][OBSERVATION_GUIDE].update(
-    {'characteristic_twenty/ch20_q3'})
 
 CLINIC_IDENTIFIER = 'facility_info/facility_cnes'
 CHARACTERISTIC = 'facility_info/HS_char'
