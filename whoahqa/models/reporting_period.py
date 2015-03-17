fd03bfb2ab9a36ef5d9d5ed5ad5b0a7bd741dc5f
geoffreymuchai <geoffreymuchai@gmail.com>
2015-03-17 17:00:14 +0300

    GM: Added doc-string to generate_form_key function

--- whoahqa/models/reporting_period.py
+++ whoahqa/models/reporting_period.py
@@ -16,13 +16,8 @@ class ReportingPeriod(Base):
     end_date = Column(Date(), nullable=False)
 
     def generate_form_key(self):
-        # generate reporting period based on the new convention
-        """
-        1may_31jul_2015 <=> 1 May - 31 Jul 2015
-        1aug_31oct_2015 <=> 1 Aug - 31 Oct 2015
-        1nov_2015_31jan_2016  <=>  1 Nov 2015 - 31 Jan 2016
-        1feb_30apr_2016 <=> 1 Feb - 30 Apr 2016
-        format string = ("%d%b%Y")
+        """ Generates the key used to filter out submissions belonging to this
+        reporting period for a clinic.
         """
         if self.end_date.year - self.start_date.year >= 1:
             period = "{}_{}".format(self.start_date.strftime("%-d%b_%Y"),
