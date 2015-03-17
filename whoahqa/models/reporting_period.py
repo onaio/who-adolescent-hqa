5208fffe753213de3d123c36b38cef7ed96bde3f
geoffreymuchai <geoffreymuchai@gmail.com>
2015-03-17 16:46:52 +0300

    GM: Added function to generate form key from reporting period start and end dates

--- whoahqa/models/reporting_period.py
+++ whoahqa/models/reporting_period.py
@@ -15,6 +15,24 @@ class ReportingPeriod(Base):
     start_date = Column(Date(), nullable=False)
     end_date = Column(Date(), nullable=False)
 
+    def generate_form_key(self):
+        # generate reporting period based on the new convention
+        """
+        1may_31jul_2015 <=> 1 May - 31 Jul 2015
+        1aug_31oct_2015 <=> 1 Aug - 31 Oct 2015
+        1nov_2015_31jan_2016  <=>  1 Nov 2015 - 31 Jan 2016
+        1feb_30apr_2016 <=> 1 Feb - 30 Apr 2016
+        format string = ("%d%b%Y")
+        """
+        if self.end_date.year - self.start_date.year >= 1:
+            period = "{}_{}".format(self.start_date.strftime("%-d%b_%Y"),
+                                    self.end_date.strftime("%-d%b_%Y"))
+        else:
+            period = "{}_{}".format(self.start_date.strftime("%-d%b"),
+                                    self.end_date.strftime("%-d%b_%Y"))
+
+        return period.lower()
+
 
 class ReportingPeriodFactory(BaseModelFactory):
 
