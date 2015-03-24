30c8b8b710622a0f07dbcde38836f47cd9933c6f
geoffreymuchai <geoffreymuchai@gmail.com>
2015-03-24 17:17:31 +0300

    GM: Load default view based on user role

--- whoahqa/views/default_views.py
+++ whoahqa/views/default_views.py
@@ -4,6 +4,7 @@ from pyramid.httpexceptions import (
 
 from whoahqa.constants import characteristics as constants
 from whoahqa.constants import permissions as perms
+from whoahqa.constants import groups
 from pyramid.view import view_config
 
 
@@ -16,7 +17,17 @@ def default(request):
         request.response.set_cookie(
             '_LOCALE_', user_setting.language)
 
-    return HTTPFound(request.route_url('clinics', traverse=()))
+    # redirect to view depending on the group which user belongs
+
+    if ona_user.group and ona_user.group.name == groups.STATE_OFFICIAL:
+        url = request.route_url('states', traverse=(ona_user.location.id))
+    elif ona_user.group.name == groups.MUNICIPALITY_MANAGER:
+        url = request.route_url(
+            'municipalities', traverse=(ona_user.location.id))
+    else:
+        url = request.route_url('clinics', traverse=())
+
+    return HTTPFound(url)
 
 
 @view_config(route_name='locale',
