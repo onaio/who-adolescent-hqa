507adfc27c551c59d26724cfafe44c391e0b0c9b
geoffreymuchai <geoffreymuchai@gmail.com>
2015-03-30 17:45:44 +0300

    GM: Added state list permission

--- whoahqa/security.py
+++ whoahqa/security.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 from pyramid.security import (
     Authenticated)
 
-from whoahqa.constants import permissions, groups
+from whoahqa.constants import permissions as perms, groups
 
 
 pwd_context = CryptContext()
@@ -11,15 +11,23 @@ pwd_context = CryptContext()
 
 GROUPS = [groups.MUNICIPALITY_MANAGER]
 
+CLINIC_MANAGER_PERMS = [perms.CAN_LIST_CLINICS,
+                        perms.CAN_VIEW_CLINICS,
+                        perms.CAN_EDIT_CLINICS]
+
+MUNICIPALITY_MANAGER_PERMS = [perms.CAN_CREATE_PERIOD,
+                              perms.CAN_ASSESS_CLINICS,
+                              perms.CAN_VIEW_MUNICIPALITY] + \
+    CLINIC_MANAGER_PERMS
+
+STATE_OFFICIAL_PERMS = MUNICIPALITY_MANAGER_PERMS + (
+    [perms.CAN_LIST_MUNICIPALITY,
+     perms.CAN_LIST_STATE,
+     perms.CAN_VIEW_STATE])
 
 GROUP_PERMISSIONS = {
-    groups.MUNICIPALITY_MANAGER: [permissions.CAN_CREATE_PERIOD,
-                                  permissions.CAN_LIST_CLINICS,
-                                  permissions.CAN_VIEW_CLINICS,
-                                  permissions.CAN_EDIT_CLINICS,
-                                  permissions.CAN_ASSESS_CLINICS,
-                                  permissions.CAN_LIST_MUNICIPALITY,
-                                  permissions.CAN_VIEW_MUNICIPALITY],
+    groups.MUNICIPALITY_MANAGER: MUNICIPALITY_MANAGER_PERMS,
+    groups.STATE_OFFICIAL: STATE_OFFICIAL_PERMS,
     groups.USER: [Authenticated],
     groups.SUPER_USER: []
 }
