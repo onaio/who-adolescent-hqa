23be64ed0a30ad2d32893769f61d4bcc991a5784
geoffreymuchai <geoffreymuchai@gmail.com>
2015-04-02 10:15:16 +0300

    GM: Fix broken permission request methods

--- whoahqa/models/base.py
+++ whoahqa/models/base.py
@@ -61,11 +61,13 @@ Base = declarative_base(cls=BaseModel)
 class RootFactory(object):
     __acl__ = [
         (Allow, groups.SUPER_USER, ALL_PERMISSIONS),
+        (Allow, groups.STATE_OFFICIAL, perms.CAN_VIEW_STATE),
         (Allow, groups.MUNICIPALITY_MANAGER, perms.CAN_CREATE_PERIOD),
         (Allow, groups.MUNICIPALITY_MANAGER, perms.CAN_LIST_CLINICS),
         (Allow, groups.MUNICIPALITY_MANAGER, perms.CAN_VIEW_CLINICS),
         (Allow, groups.MUNICIPALITY_MANAGER, perms.CAN_ASSESS_CLINICS),
         (Allow, groups.MUNICIPALITY_MANAGER, perms.CAN_EDIT_CLINICS),
+        (Allow, groups.MUNICIPALITY_MANAGER, perms.CAN_VIEW_MUNICIPALITY),
         (Allow, groups.CLINIC_MANAGER, perms.CAN_LIST_CLINICS),
         (Allow, groups.CLINIC_MANAGER, perms.CAN_VIEW_CLINICS),
         (Allow, Authenticated, perms.AUTHENTICATED),
