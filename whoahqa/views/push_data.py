9f9e4c67138bb6f0ddf215b8afd44afa5bc8b44a
geoffreymuchai <geoffreymuchai@gmail.com>
2017-06-06 16:38:47 +0300

    Add push_facilities endpoint

---
+++ whoahqa/views/push_data.py
@@ -0,0 +1,45 @@
+from pyramid.view import (
+    view_config,
+)
+from pyramid.security import NO_PERMISSION_REQUIRED
+from whoahqa.models import DBSession, State
+from ..utils import normalizeString, format_location_name as fmt
+
+
+@view_config(
+    route_name='push',
+    match_param='action=facilities',
+    request_method='GET',
+    renderer='csv',
+    permission=NO_PERMISSION_REQUIRED)
+def push_facilities(request):
+    states = DBSession.query(State).order_by("name asc").all()
+
+    header = ['CNES', 'state', 'municipality', 'facility_name']
+    rows = []
+
+    for state in states:
+        municipalities = state.children()
+
+        for municipality in municipalities:
+            clinics = municipality.children()
+
+            for clinic in clinics:
+                if clinic is not None:
+                    clinic_name = normalizeString(clinic.name)
+                    clinic_cnes = clinic.code
+                    municipality_name = normalizeString(municipality.name)
+                    state_name = normalizeString(state.name)
+                    rows.append([
+                        clinic_cnes,
+                        fmt(state_name),
+                        fmt(municipality_name),
+                        fmt(clinic_name)])
+
+    filename = 'clinics.csv'
+    request.response.content_disposition = 'attachment;filename=' + filename
+
+    return {
+        'header': header,
+        'rows': rows
+    }
