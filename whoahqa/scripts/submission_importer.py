fc24a585758d4bb5b37918697c58eae72e67093f
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-01-11 17:17:51 +0300

    Add lastest client tool form ids
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- whoahqa/scripts/submission_importer.py
+++ whoahqa/scripts/submission_importer.py
@@ -26,14 +26,14 @@ FORM_MAP = {
     'community_member_V2': 51334,
     'adolescent_in_community_V2': 51330,
     'observation_guide_V2': 51357,
-    'adolescent_client_V3': 272136,
-    'health_care_provider_V3': 272125,
-    'support_staff_V3': 272124,
-    'health_facility_manager_V3': 272128,
-    'outreach_worker_V3': 272130,
-    'community_member_V3': 272126,
-    'adolescent_in_community_V3': 272134,
-    'observation_guide_V3': 272120,
+    'adolescent_client_V3': 273344,
+    'health_care_provider_V3': 273507,
+    'support_staff_V3': 273514,
+    'health_facility_manager_V3': 273498,
+    'outreach_worker_V3': 273484,
+    'community_member_V3': 273500,
+    'adolescent_in_community_V3': 273314,
+    'observation_guide_V3': 273519,
 }
 
 
